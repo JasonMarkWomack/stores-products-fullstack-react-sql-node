6ede815e71470f68aba021783cd30b8bf0b1c307
admin <admin@admins-MacBook.local>
2017-06-02 00:18:58 -0400

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+        super();
+
+    this.state = {
+      title: 'Full Stack App',
+      companies: []
+    }
+  }
+componentDidMount(){
+  let that = this;
+//console.log('has mounted');
+fetch('http://localhost:3000/api/companies')
+.then(function (response){
+  response.json()
+  .then(function(data){
+that.setState({
+  companies: data
+})
+  })
+})
+}
+addCompany(event){
+  var that = this;
+  event.preventDefault();
+let companyData = {
+  company_name: this.refs.company_name.value,
+  customer_address: this.refs.customer_address.value,
+  ordered_item: this.refs.ordered_item.value,
+  order_id: Math.random().toFixed(4)
+};
+
+let request = new Request('http://localhost:3000/api/new-company',{
+  method:'POST',
+  headers: new Headers({'Content-Type': 'application/json'}),
+  body: JSON.stringify(companyData)
+})
+
+ let companies = that.state.companies;
+  companies.push(companyData);
+  that.setState({
+    companies: companies
+  })
+
+fetch(request)
+.then(function(response) {
+  response.json()
+  .then(function(data){
+   console.log(data)
+ 
+  })
+})
+.catch(function(err){
+  console.log(err)
+})
+}
+
   render() {
+    let title = this.state.title;
+    let companies = this.state.companies;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>{title}</h1>
+      <form ref="opsForm">
+        <input type="text" ref="company_name" placeholder="Company Name"/>
+        <input type="text" ref="customer_address" placeholder="Customer Address"/>
+        <input type="text" ref="ordered_item" placeholder="Ordered Item"/>
+        <button onClick={this.addCompany.bind(this)}>Add company</button>
+      </form>
+<ul>
+  {companies.map(company => 
+  <li key={company.order_id}>{company.company_name} {company.customer_address} {company.ordered_item}</li>)}
+</ul>
       </div>
     );
   }
--- pg-api/server.js
+++ pg-api/server.js
@@ -13,28 +13,7 @@ let pool = new pg.Pool({
     user: 'admin'
 });
 
-pool.connect((err, db, done) => {
-    if(err){
-       return console.log(err);
-    } else{
-            let company_name = 'Walmart';
-            let customer_address = 'Crazy Road';
-            let ordered_item = 'TV'
-            let order_id = Math.random().toFixed(4)
 
-        db.query('INSERT INTO ops (company_name, customer_address, ordered_item,  order_id)  VALUES($1, $2, $3, $4)',
-         [company_name, customer_address, ordered_item,  order_id],(err, table)=>{
-            if(err){
-                return console.log(err);
-
-            }else{
-                console.log('Insereted Data :)');
-db.end();
-            }
-        });
-    }
-
-}) 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -42,9 +21,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.use(function(request, response, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/api/companies', function(request,response){
+    pool.connect(function (err, db, done){
+        if(err){
+            return response.status(400).send(err)
+     
+                }
+else{
+    db.query('SELECT * FROM ops', function(err, table){
+        done();
+        if(err){
+            return response.status(400).send(err)
+        }
+        else{
+                        return response.status(200).send(table.rows)
+
+        }
+    })
+}
+                })
+            })
+  
+
+app.post('/api/new-company', function(request, response){
+   let company_name = request.body.company_name;
+   let customer_address = request.body.customer_address;
+   let ordered_item = request.body.ordered_item;
+   let order_id = request.body.order_id;
+   pool.connect((err, db, done) => {
+    if(err){
+return response.status(400).send(err);
+    } else{
+        db.query('INSERT INTO ops (company_name, customer_address, ordered_item, order_id) VALUES ($1, $2, $3, $4)',[company_name, customer_address, ordered_item, order_id],(err, table)=>{
+            if(err){
+return response.status(400).send(err);
+
+            }else{
+                console.log('INSERTED!');
+                db.end();
+
+            }
+        });
+    }
+
+}) 
+
 });
 
 app.listen(PORT, () => console.log('on' + PORT));
